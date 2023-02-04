c8d70e4e3b7930f4e987c2be6936de9b48d8376a
taron <ghlijyantaron@gmail.com>
2023-02-04 11:12:38 -0600

    f

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [appState, setAppState] = useState('login'); //login, register, user, admin
+  const [user, setUser] = useState({
+    userType: 'user', //adminl, user
+    email: '',
+    password: ''
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="login-container">
+        <h1>Sign In</h1>
+        <label>Email</label>
+        <input type='email' placeholder='Your email'></input>
+        <label>Password</label>
+        <input type='password' placeholder='Password'></input>
+        <button>Sign In</button>
+        <p></p>
+      </div>
+      <div className='register-container'>
+        <h1>Register</h1>
+        <label>Email</label>
+        <input type='email' placeholder='Your email'></input>
+        <label>Password</label>
+        <input type='password' placeholder='Password'></input>
+        <button>Register</button>
+        <p></p>
+      </div>
+      <div className='user-container'>
+        <h1>Your Library Portal</h1>
+        <div className='user-books-columns'>
+          <div className='user-my-books'>
+          
+          </div>
+
+          <div className='user-available-books'>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+
+function AvailableBook({name, count, addHandler}){
+  return (
+    <div onClick={(e) => {addHandler(e, name, count)}}>
+      <p>{name}</p>
+      <p>Left: {count} books</p>
     </div>
   );
 }
 
+function UsersBook({name, count, removeHandler}){
+  return (
+    <div onClick={(e) => {removeHandler(e, name, count)}}>
+      <p>{name}</p>
+      <p>Click to remove</p>
+    </div>
+  );
+}
+
+function AdminBook({name, count, removeHandler}){
+  return (
+    <div onClick={(e) => {removeHandler(e, name, count)}}>
+      <p>{name}</p>
+      <p>Click to remove</p>
+    </div>
+  );
+}
 export default App;
